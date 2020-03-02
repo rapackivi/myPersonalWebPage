ae964811f1398468a0bb2e9d801e618068105a2e
rapackivi <localit1981@gmail.com>
2020-03-02 21:48:17 +0200

    simon2

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,22 +1,33 @@
 var gamePattern = [];
-
+var userClickedPattern = [];
 const buttonColors = ["red", "blue", "green", "yellow"];
 
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4);
-    return randomNumber;
+    var randomChosenColor =  buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    var btnAudio = new Audio("./sounds/"+ randomChosenColor + ".mp3");
+    btnAudio.play();
 }
 
-var randomChosenColor =  buttonColors[nextSequence()];
-gamePattern.push(randomChosenColor);
 
-var buttonName = gamePattern[0];
-$("." + buttonName).addClass("pressed");
-var sourceAudio = "./sound/"+ buttonName + "mp3";
-var buttonAudio = new Audio(src="sourceAudio");
-// buttonAudio.play();
-setTimeout( () => $(buttonName).removeClass("pressed"),200);
 
-$(".btn").on("click",function(){
-
+//make our buttons to be live!
+$(".btn").on("click",function(event){
+    var btnName = event.target.id;
+    $("#"+btnName).addClass("pressed")
+    setTimeout( () => $("#"+ btnName ).removeClass("pressed"),200);
+    var sourceAudio = "./sounds/"+ btnName + ".mp3";
+    var btnAudio = new Audio(sourceAudio);
+    btnAudio.play();
+    userClickedPattern.push(btnName);
+})
+///////////////////////////////////////
+var justStarted = true;
+$(document).on("keydown", ()=>{
+    if (justStarted){
+        nextSequence();
+        justStarted = false;
+    }
 })
