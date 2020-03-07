c9a23881b8a66de78999dd4dfe2141e63cbb1e68
rapackivi <localit1981@gmail.com>
2020-03-07 15:39:02 +0200

    simon game done!

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,6 +1,7 @@
 var gamePattern = [];
 var userClickedPattern = [];
 const buttonColors = ["red", "blue", "green", "yellow"];
+var level = 0;
 
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4);
@@ -9,12 +10,18 @@ function nextSequence(){
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
     var btnAudio = new Audio("./sounds/"+ randomChosenColor + ".mp3");
     btnAudio.play();
+    level++;
+    $("h1").text("level " + level);
 }
 
 
-
+var justStarted = true;
 //make our buttons to be live!
+var lastAnswerIndex = 0;
 $(".btn").on("click",function(event){
+    if (justStarted){
+        return;
+    }
     var btnName = event.target.id;
     $("#"+btnName).addClass("pressed")
     setTimeout( () => $("#"+ btnName ).removeClass("pressed"),200);
@@ -22,12 +29,51 @@ $(".btn").on("click",function(event){
     var btnAudio = new Audio(sourceAudio);
     btnAudio.play();
     userClickedPattern.push(btnName);
+    if (checkAnswer(lastAnswerIndex)){
+        ++lastAnswerIndex;
+        console.log(lastAnswerIndex);
+    } else {
+        gameOver();
+        justStarted = true;
+        return;
+    }
+
+    if (lastAnswerIndex===level){
+        console.log("end");
+        lastAnswerIndex=0;
+        userClickedPattern = [];
+        setTimeout(nextSequence,1000);
+    }
 })
 ///////////////////////////////////////
-var justStarted = true;
+
 $(document).on("keydown", ()=>{
     if (justStarted){
         nextSequence();
         justStarted = false;
     }
 })
+
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+        console.log("success");
+        return true;
+    }else{
+        console.log("wrong");
+        return false;
+    }
+}
+
+function gameOver(){
+    level = 0;
+    lastAnswerIndex = 0;
+    userClickedPattern = [];
+    gamePattern = [];
+    var btnAudio = new Audio("./sounds/wrong.mp3");
+    btnAudio.play();
+    $("h1").text("Game over. Press any key to restart");
+    $("body").addClass("game-over")
+    setTimeout(()=>{
+        $("body").removeClass("game-over")
+    },200)
+}
