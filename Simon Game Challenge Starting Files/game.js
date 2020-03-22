180aa913202b3f259a6d8d503c1805248179c30e
rapackivi <localit1981@gmail.com>
2020-03-22 06:51:48 +0200

    simon game finished

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,6 +1,7 @@
 var gamePattern = [];
 var userClickedPattern = [];
 const buttonColors = ["red", "blue", "green", "yellow"];
+var level = 0;
 
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4);
@@ -9,17 +10,33 @@ function nextSequence(){
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
     var btnAudio = new Audio("./sounds/"+ randomChosenColor + ".mp3");
     btnAudio.play();
+<<<<<<< HEAD
     $("h1").text("level "+level);
     level++;
+=======
+    level++;
+    $("h1").text("level " + level);
+>>>>>>> c9a23881b8a66de78999dd4dfe2141e63cbb1e68
 }
 
 function checkAnswer(currentLevel){
 
+<<<<<<< HEAD
 }
 
+=======
+var justStarted = true;
+>>>>>>> c9a23881b8a66de78999dd4dfe2141e63cbb1e68
 //make our buttons to be live!
+var lastAnswerIndex = 0;
 $(".btn").on("click",function(event){
+<<<<<<< HEAD
     
+=======
+    if (justStarted){
+        return;
+    }
+>>>>>>> c9a23881b8a66de78999dd4dfe2141e63cbb1e68
     var btnName = event.target.id;
     checkAnswer(level);
     $("#"+btnName).addClass("pressed")
@@ -28,10 +45,29 @@ $(".btn").on("click",function(event){
     var btnAudio = new Audio(sourceAudio);
     btnAudio.play();
     userClickedPattern.push(btnName);
+    if (checkAnswer(lastAnswerIndex)){
+        ++lastAnswerIndex;
+        console.log(lastAnswerIndex);
+    } else {
+        gameOver();
+        justStarted = true;
+        return;
+    }
+
+    if (lastAnswerIndex===level){
+        console.log("end");
+        lastAnswerIndex=0;
+        userClickedPattern = [];
+        setTimeout(nextSequence,1000);
+    }
 })
 ///////////////////////////////////////
+<<<<<<< HEAD
 var justStarted = true;
 var level = 0;
+=======
+
+>>>>>>> c9a23881b8a66de78999dd4dfe2141e63cbb1e68
 $(document).on("keydown", ()=>{
     if (justStarted){
         nextSequence();
@@ -40,3 +76,27 @@ $(document).on("keydown", ()=>{
     }
     
 })
+
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+        console.log("success");
+        return true;
+    }else{
+        console.log("wrong");
+        return false;
+    }
+}
+
+function gameOver(){
+    level = 0;
+    lastAnswerIndex = 0;
+    userClickedPattern = [];
+    gamePattern = [];
+    var btnAudio = new Audio("./sounds/wrong.mp3");
+    btnAudio.play();
+    $("h1").text("Game over. Press any key to restart");
+    $("body").addClass("game-over")
+    setTimeout(()=>{
+        $("body").removeClass("game-over")
+    },200)
+}
