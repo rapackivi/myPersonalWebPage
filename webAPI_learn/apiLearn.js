891825a4d8d39f4d0f89b3a6c1289e7dbf719438
rapackivi <localit1981@gmail.com>
2020-03-23 22:46:34 +0200

    done 254

--- webAPI_learn/apiLearn.js
+++ webAPI_learn/apiLearn.js
@@ -3,9 +3,30 @@ const https = require("https");
 
 const app = express();
 
+const body = "https://api.openweathermap.org/data/2.5/weather?";
+const city = "q=Kharkiv";
+const units = "units=metric";
+const apiKey = "appid=11ec3a36143b9e3072bedd062674c8ef";
+const conditionCode = "http://openweathermap.org/img/wn/";
+const conditionCode2 = "@2x.png";
+
 app.get("/hello", function(req,res){
-    res.write("<img src="/images/fox.jpeg"")
-    res.send("<h1>Hello!</h1>");
+    url = body + city + "&" + units + "&" + apiKey;
+    https.get(url, function(response){
+        console.log(response.statusCode);
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescr = weatherData.weather[0].description;
+            const weatherPict = conditionCode + weatherData.weather[0].icon + conditionCode2;
+            res.write("<p>Weather in Kharkiv - " + weatherDescr + "</p>");
+            res.write("<h1>temperature in Kharkiv"+ temp +"</h1>");
+            res.write("<img src=" + weatherPict + ">");
+            res.send();
+        })
+    })
+   
+    
 })
 
 
