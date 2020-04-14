77ee033d1550b56abcda85f2329521868dcca653
rapackivi <localit1981@gmail.com>
2020-04-14 16:24:31 +0300

    com2

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -36,6 +36,6 @@ app.post("/",(req, res)=>{
 
 
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || g3000, function() {
   console.log("Server started on port 3000");
 });
