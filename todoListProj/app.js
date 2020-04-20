150ebc78330afa737229b4704b3cb3d34b9e21cd
rapackivi <localit1981@gmail.com>
2020-04-20 15:55:56 +0300

    CRUD operations added

--- todoListProj/app.js
+++ todoListProj/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 const dateUtil = require(__dirname+"/date.js");
 
+mongoose.connect("mongodb://localhost/todolistDB", { useNewUrlParser:true, useUnifiedTopology:true });
 
 const app = express();
 
@@ -10,35 +12,63 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-let items = ["Make breakfast", "go shopping", "create miracle"];
+
 let workItems = ["pass EJS tutorial", "check Test.io", "test uTest.com"]
 
 
 // ===========COMMON=================
 let [todayStr, day] = dateUtil.getDate();
 // ==================================== 
+//================DB================
+const itemSchema = mongoose.Schema({
+    name : {
+        type : String,
+        reqired : true
+    }
+});
+
+const itemModel = mongoose.model("item", itemSchema);
+
+
 
 app.get("/", (req, res)=>{
-    res.render("list", { listTitle: "main",  weekday: day%6 , date : todayStr, addedTasks : items });
+    itemModel.find({}, (err,result)=>{
+        if (!err){
+            console.log("reading success");
+            res.render("list", { 
+                listTitle: "main",  
+                weekday: day%6 , 
+                date : todayStr, 
+                addedTasks : result })
+        } else {
+            console.error.apply(err.message)
+        }
+    })
+    
 })
 
 app.get("/work", (req, res)=>{
     res.render("list", { listTitle: "work",  weekday: day%6 , date : todayStr, addedTasks : workItems });
 })
 
-app.post("/", (req,res)=>{
-    
-    newItem = req.body.newTask;
-    
-    if (req.body.list==='work'){
-        workItems.push(newItem);
-        res.redirect("/work")
-    } else {
-        items.push(newItem);
-        res.redirect("/");
-    }
+app.post("/delete", (req,res)=>{
+    const id = req.body.checkbox;
+    itemModel.findByIdAndDelete(id, (err)=> console.log(err||"successfuly deleted!"));
+    res.redirect("/");
 })
 
+app.post("/", (req,res)=>{
+    
+    const nameNewItem = req.body.newTask;
+    const newItem = new itemModel({
+        name : nameNewItem
+    });
+    newItem.save();
+    res.redirect("/")
+    
+})
+
+app.get("/lists/:param")
 app.get("/about", (req,res)=>{
     res.render("about")
 })
---
+++ todoListProj/fillingDB.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/todolistDB", { useNewUrlParser:true , useUnifiedTopology:true }, (err)=>{
+    if (!err){
+        console.log("connect success")
+    } else {
+        console.error.apply(err.message)
+    }
+})
+
+
+
+////first fill//////////////////////
+const itemSchema = mongoose.Schema({
+    name : {
+        type : String,
+        reqired : true
+    }
+});
+
+const itemModel = mongoose.model("item", itemSchema);
+
+const Item1 = new itemModel(
+    {
+        name : "to kill the Bill"
+    }
+)
+
+const Item2 = new itemModel(
+    {
+        name : "to find a pencil for Masha"
+    }
+)
+
+const Item3 = new itemModel(
+    {
+        name : "to find a pencil for Masha"
+    }
+)
+
+itemModel.insertMany([Item1,Item2,Item3],((err)=>{
+    if (!err){
+        console.log("inserted success");
+        mongoose.disconnect((err)=>{
+            if (!err){
+                console.log("connect success")
+            } else {
+                console.error.apply(err.message)
+            }
+        });
+    } else {
+        console.error.apply(err.message)
+    }
+}))
